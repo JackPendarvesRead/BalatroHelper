2aa70db14b19fdf3ac5ebf1b7c3ce5c9e00b6c8a
Jack Read <jackpendarvesread@gmail.com>
2024-03-08 23:04:55 +0000

    Add project files.

---
+++ BalatroHelper.Logic/Entities/IEvaluatable.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BalatroHelper.Logic.Entities
+{
+    public interface IEvaluatable
+    {
+        int Chips { get; set; }
+
+        int Mult { get; set; }
+
+        float MultMultiplier { get; set; }
+
+        abstract int Evaluate();
+    }
+}
